3316e681116e06123c4412931bbe073f5a10e654
7iendat <hunterdev03@gmail.com>
2025-03-04 18:46:46 +0700

    create product api

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.model.js";
+import redis from "../libs/redis.js";
 
 export const getAllProducts = async (req, res) => {
   try {
@@ -12,4 +13,26 @@ export const getAllProducts = async (req, res) => {
   }
 };
 
-export const getFeaturedProducts = async (req, res) => {};
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    // not in redis
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts)
+      return res.status(404).json({ message: "No featured products found" });
+
+    // store in redis
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+    return res.json(featuredProducts);
+  } catch (error) {
+    console.log("Error in get featuredProducts", error.message);
+    return res
+      .status(500)
+      .json({ message: "Server error something", error: error.message });
+  }
+};
